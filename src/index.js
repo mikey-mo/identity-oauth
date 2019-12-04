dae3a1878a43593df0de4c0e78475be9dbe4b9da
Anthony Keating <anthonyk1225@gmail.com>
2019-12-04 16:32:24 -0500

    added new screen for verifying code

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import './index.css';
 import App from './App';
 import Permissions from './Screens/Permissions';
 import Identifiers from './Screens/Identifiers';
+import VerifyCode from './Screens/VerifyCode';
 import * as serviceWorker from './serviceWorker';
 
 const navigation = (
   <Router>
     <Route exact path="/" component={App} />
-    <Route path="/auth/identifiers" component={Identifiers} />
+    <Route path="/auth/identifiers/:identifier" component={Identifiers} />
+    <Route path="/auth/verify" component={VerifyCode} />
     <Route path="/auth/permissions" component={Permissions} />
   </Router>
 );
