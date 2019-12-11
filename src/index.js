e4c2187e415b1b48e582cf3a6ebe8b5444feb51f
Anthony Keating <anthonyk1225@gmail.com>
2019-12-11 12:58:25 -0500

    added spinner for requests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
@@ -6,18 +6,53 @@ import App from './App';
 import Permissions from './Screens/Permissions';
 import VerifyCode from './Screens/VerifyCode';
 import * as serviceWorker from './serviceWorker';
+import { WhisperSpinner } from "react-spinners-kit";
 
-const navigation = (
-  <Router>
-    <Route exact path="/:type" component={App} />
-    <Route path="/auth/verify" component={VerifyCode} />
-    <Route path="/auth/permissions" component={Permissions} />
-    <Route path="/auth/verified"><div><p>Verified</p></div></Route>
-    <Route path="/auth/cancelled"><div><p>Cancelled</p></div></Route>
-  </Router>
-);
+class Navigation extends Component {
+  state = { visible: false };
 
-ReactDOM.render(navigation, document.getElementById('root'));
+  toggleLoader = visible => this.setState({ visible })
+
+  render() {
+    const { visible } = this.state;
+
+    return (
+      <div>
+      <Router>
+        <Route exact path="/:type" component={App} />
+        <Route path="/auth/verify" component={VerifyCode} />
+        <Route path="/auth/permissions" component={Permissions} />
+        <Route path="/auth/verified"><div><p>Verified</p></div></Route>
+        <Route path="/auth/cancelled"><div><p>Cancelled</p></div></Route>
+      </Router>
+      { visible &&
+        <div
+          style={{
+            backgroundColor: 'grey',
+            opacity: 0.6,
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <WhisperSpinner
+            size={50}
+            color="white"
+            loading
+          />
+        </div>
+      }
+    </div>
+    );
+  }
+}
+
+ReactDOM.render(<Navigation />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
